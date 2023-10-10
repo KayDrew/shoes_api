97425d9f00eb394fa093b7fc73dbe4dae072c137
KayDrew <kdtshehla@gmail.com>
2023-10-10 06:09:55 -0700

    initial commit

---
+++ database.js
@@ -0,0 +1,94 @@
+export default function dbQueries(db){
+
+/**async function create(){
+
+try{
+	
+await db.none("CREATE TABLE shoes(id INT PRIMARY KEY NOT NULL,color VARCHAR(255),brand VARCHAR(255),price INT,size INT,in_stock INT)");
+console.log("successfully  created");
+
+}catch(err){
+
+console.log(err);
+}
+
+}*/
+
+
+async function  addShoes(id,color,brand,price,size,in_stock){
+
+    try{
+
+                await db.none("INSERT INTO shoes (id,color,brand,price,size,in_stock) VALUES ($1,$2,$3,$4,$5,$6)",[id,color,brand,price,size,in_stock]);
+
+                console.log("successfully  inserted");
+     }catch(err){
+	
+                 console.log(err);
+
+      }
+}
+
+
+async function  getAll(){
+
+    try{
+            let shoes=db.manyOrNone("SELECT * FROM shoes");
+            return shoes;
+    }catch(err){
+    	
+           console.log(err);
+   }
+}
+
+
+async function  getBrand(brand){
+
+    try{
+            let shoes=db.manyOrNone("SELECT * FROM shoes WHERE brand=$1",brand);
+            return shoes;
+          
+    }catch(err){
+    	
+           console.log(err);
+   }
+}
+
+
+async function  getBrand(brand){
+
+    try{
+            let shoes=db.manyOrNone("SELECT * FROM shoes WHERE brand=$1",brand);
+            return shoes;
+          
+    }catch(err){
+    	
+           console.log(err);
+   }
+}
+
+
+async function  getSize(size){
+
+    try{
+            let shoes=db.manyOrNone("SELECT * FROM shoes WHERE size=$1",size);
+            return shoes;
+          
+    }catch(err){
+    	
+           console.log(err);
+   }
+}
+
+
+
+return{
+	addShoes,
+	getAll,
+	getBrand,
+	getSize
+//create,
+
+}
+
+}
---
+++ index.js
@@ -0,0 +1,47 @@
+import 'dotenv/config';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { engine } from 'express-handlebars';
+import flash from 'express-flash';
+import session from 'express-session';
+import pkg from 'pg-promise';
+import dbQueries from './database.js';
+import services from './services/shoes.js';
+
+const app = express();
+
+app.use(express.static('public'));
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+app.use(express.static('public'));
+app.use(express.static('images'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(session({
+  secret: "no secret",
+  resave: false,
+  saveInitialized: false
+}));
+app.use(flash());
+
+const connectionString=process.env.URL;
+const Pool= pkg();
+const db=Pool ({connectionString ,
+ssl: true 
+});
+
+const query= dbQueries(db);
+const service= services(query);
+
+app.get("/", service.showAll);
+app.get("/api/shoes", service.getAll);
+app.get("/api/shoes/brand/:brandname", service.getBrand);
+app.get("/api/shoes/size/:size", service.getSize);
+
+const PORT= process.env.PORT||5432;
+
+app.listen(PORT,function(){
+    console.log("App starting on port "+PORT);
+});
