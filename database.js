7efbfd2cdf1cd8abff79c4849b7cec4ad9ed8407
Kabelo Drew Tshehla <66612725+KayDrew@users.noreply.github.com>
2023-10-19 16:29:50 +0200

    Update database.js

--- database.js
+++ database.js
@@ -46,7 +46,7 @@ async function  addShoes(color,brand,price,size,in_stock,image){
                 await db.none("INSERT INTO shoes (id,color,brand,price,size,in_stock,image) VALUES (DEFAULT,$1,$2,$3,$4,$5,$6)",[color,brand,price,size,in_stock,image]);
 
                 
-	    return "Successfully  added new stock",
+	    return "Successfully  added new stock";
          }catch(err){
 	
                  console.log(err);
