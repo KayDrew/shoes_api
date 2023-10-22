bba82c240f642695fa6bda27ef0920376bbf5b49
KayDrew <kdtshehla@gmail.com>
2023-10-22 03:18:02 -0700

    added condition

--- database.js
+++ database.js
@@ -31,8 +31,8 @@ export default function dbQueries(db){
                 try{
                       await db.none("INSERT INTO cart_items(cart_code,id,qty) VALUES($1,$2,$3)",[cart_code,shoesId,qty]);
 
-                      let result= await db.manyOrNone("SELECT shoes.brand, shoes.color,shoes.price,shoes.image, cart_items.qty FROM cart_items JOIN shoes on cart_items.id=shoes.id");
-        s
+                      let result= await db.manyOrNone("SELECT shoes.brand, shoes.color,shoes.price,shoes.image,cart_items.qty FROM cart_items JOIN shoes on cart_items.id=shoes.id WHERE cart_code=$1",cart_code);
+        
                         return result;
         
                 }catch(err){
